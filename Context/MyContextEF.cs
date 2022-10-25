de45a7560c538af4644eafd507b3743ff8d526a0
Jose Luis Diaz Galvan <jlgalvan@rutatrol.com>
2022-10-25 20:22:59 -0300

    asasdasd

---
+++ Context/MyContextEF.cs
@@ -0,0 +1,34 @@
+﻿using NET.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace NET_Framework.Context
+{
+	public class MyContextEF : DbContext
+	{
+        public MyContextEF() : base("Data Source=localhost; Initial Catalog=NET_Framework;Integrated Security=yes")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Adress> Adresses { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Company> Companys { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<SaleLine> SaleLines { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
